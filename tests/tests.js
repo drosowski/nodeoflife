460f3ea17099f6f4a0c94c53776d07f1a1c78e7e
Daniel Rosowski <rosowski@smartsquare.de>
2013-02-03 19:00:16 +0100

    Added setup for tests using qunit modules

--- tests/tests.js
+++ tests/tests.js
@@ -1,9 +1,21 @@
-test("board tests", function() {
-	var Board = require('../board.js');
-	var board = new Board();
+var Board = require('../board.js');
+var board;
 
-	deepEqual(board.population, [[0,0,0],[1,1,1],[0,0,0]]);
+QUnit.module( "CELL rule tests; cell with ...", {
+	setup: function() {
+		var Board = require('../board.js');
+		board = new Board();
+	}
+});
+
+test("...EXACTLY THREE neighbours REVIVES", function() {
 	board.evolve();
 	deepEqual(board.population, [[0,1,0],[0,1,0],[0,1,0]]);
+});
+
+test("...LESS THAN TWO neighbours DIES", function() {
+	board.population = [[0,0,0],[0,1,0],[0,1,0]];
+	board.evolve();
+	deepEqual(board.population, [[0,0,0],[0,0,0],[0,0,0]]);
 });
 
